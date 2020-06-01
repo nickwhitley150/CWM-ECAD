1e07e65b039e51fe793468ffae798b6839b2132b
Cloud User <centos@ip-172-31-14-64.eu-west-2.compute.internal>
2020-06-01 11:51:32 +0000

    ex2 not good

--- Ex2/top.v
+++ Ex2/top.v
@@ -22,11 +22,19 @@
 
 module mux(
     //Todo: define inputs here
+	input a,
+	input b,
+	input sel,
+	output out
     );
     
     //Todo: define registers and wires here
+	wire out;
 
 
-    //Todo: define your logic here                 
+    //Todo: define your logic here   
+	wire[1:0] sel={sel};
+	assign #5 out = (sel==1'b0) ? a:
+	b;           
       
 endmodule
