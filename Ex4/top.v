9b680223e05dfb4741a672f7a2c6fc639a9fec31
Cloud User <centos@ip-172-31-14-64.eu-west-2.compute.internal>
2020-06-04 09:15:49 +0000

    complete solution

--- Ex4/top.v
+++ Ex4/top.v
@@ -15,4 +15,39 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module counter(
+    //Todo: add ports 
+	input clk
+	input rst
+	input button
+	output reg [2:0] throw
+
+
+    );
+                    
+    //Todo: add registers and wires, if needed
+
+	
+
+
+
+
+
+    //Todo: add user logic
+always @(posedge clk) 
+if (rst == 0 )
+	if (throw==3'b000|throw==3'b111)
+		throw<=3'b001;
+	else 
+	if (button==1)
+		if (throw==3'b110)
+		throw<=3'b001;
+		else 
+		throw <= throw+1;
+ else
+throw <= 3'b000;
+
+	
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,78 @@
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+//Todo: Parameters
+
+parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+
+reg clk;
+reg err;
+reg rst;
+reg button;
+reg direction;
+reg [2:0]throw_prev;
+wire [2:0]throw;
+
+
+//Todo: Clock generation
+
+initial
+    begin
+       clk = 1'b0
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//Todo: User logic
+
+ initial begin
+        rst =0 ;
+	button=1'b0;
+	err=0;
+	throw_prev=3'b111;
+	throw=3'b111;
+       
+end
+forever begin
+         #CLK_PERIOD*10
+         if ((rst==1)&&(counter_out_prev!=8'b000000))
+	begin
+        $display("***TEST FAILED! rst==%d, enable==%d, direction==%d, counter_out==%d***", rst, enable, direction, counter_out, counter_out_prev);
+        err =1;
+          end 
+	 if ((enable==0)&&(counter_out_prev!=))
+      end
+
+
+    
+//Todo: Finish test, check for success
+
+initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+//Todo: Instantiate counter module
+
+counter(
+    //Todo: add ports 
+    .clk (clk),
+    .rst (rst),
+    .enable (enable),
+    .direction (direction),
+    ,couter_out (counter_out)
+
+ 
+
+    );
+ 
+endmodule 
