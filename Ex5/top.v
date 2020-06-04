f77074b3076acaec9ec78fa2d32899e5c4a534a7
Cloud User <centos@ip-172-31-14-64.eu-west-2.compute.internal>
2020-06-04 11:53:44 +0000

    ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,45 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module traffic(
+    //Todo: add ports 
+	input clk
+	output red
+	output amber
+	output green
+
+
+    );
+                    
+    //Todo: add registers and wires, if needed
+
+	reg[1:0]state;
+
+
+
+    //Todo: add user logic
+
+assign red = state[1]
+assign amber = state[0]
+assign green = (state == 2'b10)? (1'b1):(1'b0);
+
+always @(posedge clk)
+begin
+
+if (state==2'b11)
+	state <= 2'b0;
+else 
+	state <= state+1;
+
+$display("lights=%h red=%h amber=%h green=%h", state, red, amber, green);  
+
+end
+initial 
+	begin
+		state = 2'b0;
+	end
+
+	
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,4 +6,28 @@
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module traffic_test();
+
+parameter CLK_PERIOD = 10;
+reg clk;
+reg err;
+reg red_prev;
+reg amber_prev;
+reg green_prev;
+wire red;
+wire amber;
+wire green;
+
+//clock
+initial
+    begin
+       clk = 1'b0
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+traffic traffic(clk, red, amber, green);
+
+endmodule
